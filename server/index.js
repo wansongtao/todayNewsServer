4dbbfc469f9eb4df05b7211296a03a8a55e351ac
万松涛 <2294215581@qq.com>
2020-11-23 09:35:14 +0800

    创建服务器实例并添加错误处理

--- server/index.js
+++ server/index.js
@@ -5,3 +5,27 @@
  */
 
  const INDEX = {};
+
+ //1.引入express框架
+ INDEX.express = require('express');
+
+ //2.创建服务器实例
+ INDEX.webApp = INDEX.express();
+
+ //3.监听端口
+ INDEX.webApp.listen(5050, err => {
+    if(err) {
+        console.error('listen(): ', err);
+    }
+ });
+
+ //4.服务器错误处理
+ INDEX.webApp.use((err, req, res, next) => {
+    console.error('server error: ', err);
+    res.status(500).send({statusCode: 500, message: '服务器错误'});
+ });
+
+ //5.资源路径错误处理
+ INDEX.webApp.use((req, res) => {
+    res.status(404).send({statusCode: 404, message: '找不到该资源'});
+ });
