2ab2ad8d787d6743b3ad19bfbd6af10743486dae
万松涛 <2294215581@qq.com>
2020-11-27 12:22:06 +0800

    优化代码

--- src/controller/processing.js
+++ src/controller/processing.js
@@ -4,10 +4,6 @@
  * @date 2020-11
  */
 
-const {
-    resolve
-} = require('path');
-
 const PROCESS = {};
 
 PROCESS.database = require('../database/database');
@@ -49,15 +45,80 @@ PROCESS.checkUserName = async (userName) => {
 };
 
 /**
- * @description 登录逻辑处理
- * @param {object} req.body 一个请求体参数对象
- * @returns 返回 {statusCode: 200, data: {}, message: '成功'}
+ * @description 处理用户上传的文件
+ * @param {*} req 请求对象
+ * @returns 返回一个期约对象，解决状态返回{statusCode: 200,message: '图片上传成功'}
  */
-PROCESS.login = async ({
-    userName,
-    userPwd
-}) => {
+PROCESS.formidableImg = (req) => {
+    return new Promise((resolve, reject) => {
+        //创建formidable.IncomingForm()对象，需要引入formidable模块
+        let form = new PROCESS.formidable();
+
+        //设置表单域的编码
+        form.encoding = 'utf-8';
+
+        //设置上传文件存放的文件夹路径
+        form.uploadDir = PROCESS.path.join(__dirname, '../upload/img');
+
+        //保留上传文件的后缀名
+        form.keepExtensions = true;
+
+        form.maxFieldsSize = 0.5 * 1024 * 1024;
+
+        //设置上传文件的大小，500kb，默认2M
+        form.maxFileSize = 0.5 * 1024 * 1024;
+
+        form.onPart = (part) => {
+            //必须要调用form.parse(req, callback)方法 form.parse(req) => form.onPart() => form.parse()的回调callback
+            //只允许上传照片
+            if (part.mime.indexOf('image') !== -1) {
+                // used internally, please do not override!
+                form.handlePart(part); //调用这个方法才可以将文件存入磁盘
+            } else {
+                resolve({
+                    statusCode: 309,
+                    message: '只能上传照片'
+                });
+            }
+        };
+
+        form.parse(req, (err, fields, files) => {
+            if (err) {
+                console.error('PROCESS.formidableImg() => form.parse(): ', err);
+                resolve({
+                    statusCode: 308,
+                    message: '图片上传错误'
+                });
+            } else {
+                //如果用户上传的不是照片，files为空对象 => {}
+                if (files.file !== undefined) {
+                    const pathName = '/upload/img/' + PROCESS.path.basename(files.file.path);
+
+                    resolve({
+                        statusCode: 200,
+                        data: {
+                            imgUrl: pathName
+                        },
+                        message: '上传成功'
+                    });
+                }
+            }
+        });
+    });
+};
+
+/**
+ * @description 登录逻辑处理
+ * @param {*} req 请求对象
+ * @param {*} res 响应对象
+ */
+PROCESS.login = async (req, res) => {
+    let {
+        userName,
+        userPwd
+    } = req.body;
     let message = {};
+
     if (typeof userName !== 'string' || typeof userPwd !== 'string') {
         message = {
             statusCode: 300,
@@ -68,22 +129,12 @@ PROCESS.login = async ({
         message = await PROCESS.checkUserName(userName);
 
         if (message.statusCode == 306) {
-            //查询密码是否正确
+            //已注册，验证密码是否正确
             let queryStr = 'select userId from useraccount where userName = ? and userPwd = ?';
 
             let data = await PROCESS.database.query(queryStr, [userName, userPwd]);
 
-            if (data === false) {
-                message = {
-                    statusCode: 401,
-                    message: '服务器繁忙，请稍后再试'
-                };
-            } else if (data.length === 0) {
-                message = {
-                    statusCode: 302,
-                    message: '密码错误'
-                };
-            } else {
+            if (data[0] !== undefined) {
                 //生成token
                 const token = PROCESS.token.createToken(data[0].userId);
 
@@ -101,29 +152,41 @@ PROCESS.login = async ({
                         message: '登录成功'
                     };
                 }
+
+            } else if (data.length === 0) {
+                message = {
+                    statusCode: 302,
+                    message: '密码错误'
+                };
+            } else {
+                message = {
+                    statusCode: 401,
+                    message: '服务器繁忙，请稍后再试'
+                };
             }
         }
 
     }
 
-    return message;
+    res.send(message);
 };
 
 /**
  * @description 注册逻辑处理
- * @param {object} req.body 请求体参数对象
- * @returns 返回 {statusCode: 200, data: {}, message: '成功'}
+ * @param {*} req 请求对象
+ * @param {*} res 响应对象
  */
-PROCESS.register = async ({
-    userName,
-    userPwd,
-    nickName
-}) => {
+PROCESS.register = async (req, res) => {
     let message = {
         statusCode: '400',
-        data: {},
         message: '服务器繁忙，请稍后再试'
     };
+
+    let {
+        userName,
+        userPwd,
+        nickName
+    } = req.body;
 
     if (typeof userName !== 'string' || typeof userPwd !== 'string' || typeof nickName !== 'string') {
         message = {
@@ -135,33 +198,37 @@ PROCESS.register = async ({
         let regExp = /^[a-zA-Z][a-zA-Z0-9]{2,5}$/;
 
         if (!regExp.test(userName)) {
-            return {
+            res.send({
                 statusCode: 303,
-                message: '用户名格式错误'
-            };
+                message: '请输入3-6位字母/数字组合且以字母开头的用户名'
+            });
+            return;
         }
 
         regExp = /^[a-zA-Z][\w]{5,15}$/;
         if (!regExp.test(userPwd)) {
-            return {
+            res.send({
                 statusCode: 304,
-                message: '密码格式错误'
-            };
+                message: '请输入6-16位字母、数字、下划线组合且以字母开头的密码'
+            });
+            return;
         }
 
         regExp = /^[\u4e00-\u9fa5]{2,7}$/;
         if (!regExp.test(nickName)) {
-            return {
+            res.send({
                 statusCode: 305,
-                message: '昵称格式错误'
-            };
+                message: '请输入2-7位中文昵称'
+            });
+            return;
         }
 
         //判断用户账号是否已注册
         message = await PROCESS.checkUserName(userName);
 
         if (message.statusCode != 301) {
-            return message;
+            res.send(message);
+            return;
         }
 
         let isSuccess = await PROCESS.database.register('insert into useraccount set ?', {
@@ -182,22 +249,26 @@ PROCESS.register = async ({
         }
     }
 
-    return message;
+    res.send(message);
 };
 
 /**
  * @description 获取所有栏目
- * @returns 返回{statusCode: 200, data: {category: [{ id: 1, name: "游戏" }]}, message: ''}
+ * @param {*} req 请求对象
+ * @param {*} res 响应对象
  */
-PROCESS.category = async () => {
+PROCESS.category = async (req, res) => {
     let message = {};
     let queryStr = 'select categoryId, categoryName from category order by categoryHot desc';
     let data = await PROCESS.database.query(queryStr);
 
-    if (data === false) {
+    if (data[0] !== undefined) {
         message = {
-            statusCode: 401,
-            message: '服务器繁忙，请稍后再试'
+            statusCode: 200,
+            data: {
+                category: data
+            },
+            message: '获取成功'
         };
     } else if (data.length === 0) {
         message = {
@@ -206,49 +277,49 @@ PROCESS.category = async () => {
         };
     } else {
         message = {
-            statusCode: 200,
-            data: {
-                category: data
-            },
-            message: '获取成功'
+            statusCode: 401,
+            message: '服务器繁忙，请稍后再试'
         };
     }
 
-    return message;
+    res.send(message);
 };
 
 /**
  * @description 获取新闻列表
- * @param {Object} param0 {categoryId, currentPage, pageSize} 栏目id、当前页码、每页新闻条数，可选参数
- * @returns 返回 {statusCode: 200, data: {newList: []}, message: '成功'}
+ * @param {*} req 请求对象
+ * @param {*} res 响应对象
  */
-PROCESS.getNewList = async ({
-    categoryId,
-    currentPage,
-    pageSize
-}) => {
+PROCESS.getNewList = async (req, res) => {
     let message = {
         statusCode: '400',
         data: {},
         message: '服务器繁忙，请稍后再试'
     };
 
+    let {
+        categoryId,
+        currentPage,
+        pageSize
+    } = req.query;
+
     let queryStr = '',
         queryParams = [];
 
     if (categoryId === undefined) {
         //获取所有新闻列表
-        queryStr = 'SELECT newsId, newsTitle, newsCover, commentNums from newslists limit ? offset ?';
+        queryStr = 'SELECT newsId, nickName, newsTitle, newsCover, commentNums from newlists limit ? offset ?';
     } else {
         //获取对应栏目的新闻列表
         if (parseInt(categoryId) === NaN) {
-            return {
+            res.send({
                 statusCode: '300',
                 message: '请求参数错误'
-            };
+            });
+            return;
         }
 
-        queryStr = `SELECT newsId, newsTitle, newsCover, commentNums from newslists where 
+        queryStr = `SELECT newsId, nickName, newsTitle, newsCover, commentNums from newlists where 
         newsId in (select newsId from news_category where categoryId = ?) limit ? offset ?`;
         queryParams.push(parseInt(categoryId));
     }
@@ -285,38 +356,39 @@ PROCESS.getNewList = async ({
         };
     }
 
-    return message;
+    res.send(message);
 };
 
 /**
  * @description 获取用户详情
- * @param {string} token 接收token字符串
- * @returns 返回 {statusCode: 200, data: {}, message: '成功'}
+ * @param {*} req 请求对象
+ * @param {*} res 响应对象
  */
-PROCESS.getUserDetails = async ({
-    authorization
-}) => {
+PROCESS.getUserDetails = async (req, res) => {
     let message = {
         statusCode: '400',
         data: {},
         message: '服务器繁忙，请稍后再试'
     };
 
+    let {
+        authorization
+    } = req.headers;
+
     if (typeof authorization !== 'string') {
         message = {
             statusCode: '300',
-            data: {},
             message: '请求头错误'
         };
     } else {
         let userId = PROCESS.token.verifyToken(authorization);
 
         if (userId === false) {
-            return {
+            res.send({
                 statusCode: '500',
-                data: {},
                 message: '用户身份过期，请重新登录'
-            };
+            });
+            return;
         }
 
         let queryStr = `select userName, nickName, head_img, gender from userdetails as ud, 
@@ -324,7 +396,7 @@ PROCESS.getUserDetails = async ({
 
         let data = await PROCESS.database.query(queryStr, [parseInt(userId)]);
 
-        if (data !== false && data.length > 0) {
+        if (data[0] !== undefined) {
             message = {
                 statusCode: '200',
                 data: {
@@ -335,55 +407,59 @@ PROCESS.getUserDetails = async ({
         } else if (data === false) {
             message = {
                 statusCode: '401',
-                data: {},
                 message: '服务器繁忙，请稍后再试'
             };
         } else {
             message = {
                 statusCode: '404',
-                data: {},
                 message: '服务器繁忙，请稍后再试'
             };
         }
     }
 
-    return message;
+    res.send(message);
 };
 
 /**
  * @description 修改用户信息
- * @param {Object} param0 请求头
- * @param {Object} param1 请求体
- * @returns 返回 {statusCode: 200, message: '成功'}
+ * @param {*} req 请求对象
+ * @param {*} res 响应对象
  */
-PROCESS.editUserInfo = async ({
-    authorization
-}, {
-    nickName,
-    head_img,
-    gender
-}) => {
+PROCESS.editUserInfo = async (req, res) => {
     let message = {
         statusCode: '400',
         message: '服务器繁忙，请稍后再试'
     };
 
+    let {
+        authorization
+    } = req.headers;
+
+
     if (typeof authorization !== 'string') {
-        return {
+        res.send({
             statusCode: '300',
             message: '请求头错误'
-        };
+        });
+        return;
     }
 
     //验证token并获取用户id
     let userId = PROCESS.token.verifyToken(authorization);
 
     if (userId === false) {
-        return {
+        res.send({
             statusCode: '500',
             message: '用户身份过期，请重新登录'
-        };
+        });
+        return;
     }
+
+    let {
+        nickName,
+        head_img,
+        gender
+    } = req.body;
 
     let data = false;
     if (typeof nickName === 'string') {
@@ -391,10 +467,11 @@ PROCESS.editUserInfo = async ({
         let regExp = /^[\u4e00-\u9fa5]{2,7}$/;
 
         if (!regExp.test(nickName)) {
-            return {
+            res.send({
                 statusCode: '305',
-                message: '昵称格式错误'
-            };
+                message: '请输入2-7位中文昵称'
+            });
+            return;
         }
 
         let queryStr = 'update userdetails set nickName = ? where userId = ?;'
@@ -405,10 +482,11 @@ PROCESS.editUserInfo = async ({
         let regExp = /^[01]{1}$/;
 
         if (!regExp.test(gender)) {
-            return {
+            res.send({
                 statusCode: '307',
                 message: '性别错误'
-            };
+            });
+            return;
         }
 
         let queryStr = 'update userdetails set gender = ? where userId = ?;'
@@ -420,58 +498,61 @@ PROCESS.editUserInfo = async ({
 
         data = await PROCESS.database.update(queryStr, [head_img, userId]);
     } else {
-        return {
+        res.send({
             statusCode: '300',
             message: '参数错误'
-        };
+        });
+        return;
     }
 
-    if (data === false) {
-        message = {
-            statusCode: '401',
-            message: '服务器繁忙，请稍后再试'
-        };
-    } else {
+    if (data !== false) {
         message = {
             statusCode: '200',
             message: '修改成功'
         };
+    } else {
+        message = {
+            statusCode: '401',
+            message: '服务器繁忙，请稍后再试'
+        };
     }
 
-    return message;
+    res.send(message);
 };
 
 /**
  * @description 修改密码
- * @param {Object} param0 请求头对象，应该包含{authorization}这一个参数
- * @param {Object} param1 请求体对象，应该包含{oldPwd, newPwd}这两个参数
- * @returns 返回 {statusCode: 200, message: '成功'}
+ * @param {*} req 请求对象
+ * @param {*} res 响应对象
  */
-PROCESS.updatePwd = async ({
-    authorization
-}, {
-    oldPwd,
-    newPwd
-}) => {
+PROCESS.updatePwd = async (req, res) => {
     let message = {
         statusCode: 400,
         message: '服务器繁忙，请稍后再试'
     };
 
+    let {authorization} = req.headers,
+    {
+        oldPwd,
+        newPwd
+    } = req.body;
+
     if (typeof authorization !== 'string' || typeof oldPwd !== 'string' || typeof newPwd !== 'string') {
-        return {
+        res.send({
             statusCode: 300,
             message: '请求参数错误'
-        };
+        });
+        return;
     }
 
     let userId = PROCESS.token.verifyToken(authorization);
 
     if (userId === false) {
-        return {
+        res.send({
             statusCode: 500,
             message: '用户身份过期，请重新登录'
-        };
+        });
+        return;
     }
 
     //判断用户原密码是否正确
@@ -479,7 +560,7 @@ PROCESS.updatePwd = async ({
 
     let data = await PROCESS.database.query(queryStr, [userId, oldPwd]);
 
-    if (data !== false && data.length > 0) {
+    if (data[0] !== undefined) {
         //原密码正确
         queryStr = 'update useraccount set userPwd = ? where userId = ?';
 
@@ -509,14 +590,13 @@ PROCESS.updatePwd = async ({
         };
     }
 
-    return message;
+    res.send(message);
 };
 
 /**
  * @description 上传图片
- * @param {Object} req 请求对象
- * @param {Object} res 响应对象
- * @returns 返回{statusCode: 200, data: {imgUrl: pathName}, message: '上传成功'}
+ * @param {*} req 请求对象
+ * @param {*} res 响应对象
  */
 PROCESS.uploadImg = async (req, res) => {
     let message = {
@@ -544,59 +624,7 @@ PROCESS.uploadImg = async (req, res) => {
         return;
     }
 
-    message = await (() => {
-        return new Promise((resolve, reject) => {
-            //创建formidable.IncomingForm()对象，需要引入formidable模块
-            let form = new PROCESS.formidable();
-
-            //设置表单域的编码
-            form.encoding = 'utf-8';
-
-            //设置上传文件存放的文件夹路径
-            form.uploadDir = PROCESS.path.join(__dirname, '../upload/img');
-
-            //保留上传文件的后缀名
-            form.keepExtensions = true;
-
-            form.maxFieldsSize = 0.5 * 1024 * 1024;
-
-            //设置上传文件的大小，500kb，默认2M
-            form.maxFileSize = 0.5 * 1024 * 1024;
-
-            form.onPart = (part) => {
-                //只允许上传照片
-                if (part.mime.indexOf('image') !== -1) {
-                    // used internally, please do not override!
-                    form.handlePart(part);  //调用这个方法才可以将文件存入磁盘
-                }else {
-                    resolve({statusCode: 309, message: '只能上传照片'});
-                }
-            };
-
-            form.parse(req, (err, fields, files) => {
-                if (err) {
-                    console.error('form.parse(): ', err);
-                    resolve({
-                        statusCode: 308,
-                        message: '图片上传错误'
-                    });
-                } else {
-                    //如果用户上传的不是照片，files为空对象 => {}
-                    if (files.file !== undefined) {
-                        const pathName = '/upload/img/' + PROCESS.path.basename(files.file.path);
-                        
-                        resolve({
-                            statusCode: 200,
-                            data: {
-                                imgUrl: pathName
-                            },
-                            message: '上传成功'
-                        });
-                    }
-                }
-            });
-        });
-    })();
+    message = await PROCESS.formidableImg(req);
 
     res.send(message);
 };
--- src/router/router.js
+++ src/router/router.js
@@ -13,69 +13,35 @@ ROUTER.formidable = require('formidable');
 //创建路由实例
 ROUTER.myRouter = ROUTER.express.Router();
 
+// req.query
+// req.body
+// req.headers['Authorization']
 //返回静态资源
 ROUTER.myRouter.use('/upload', ROUTER.express.static(ROUTER.path.join(__dirname, '../../src/upload')));
 
 //定义登录接口
-ROUTER.myRouter.post('/login', async (req, res) => {
-    // req.query
-    // req.body
-    // req.headers['Authorization']
-    let message = {statusCode: 400, message: '服务器繁忙，请稍后再试'};
-    message = await ROUTER.processing.login(req.body);
-    res.send(message);
-});
+ROUTER.myRouter.post('/login', ROUTER.processing.login);
 
 //定义注册接口
-ROUTER.myRouter.post('/register', async (req, res) => {
-    let message = {statusCode: 400, message: '服务器繁忙，请稍后再试'};
-    message = await ROUTER.processing.register(req.body);
-    res.send(message);
-});
+ROUTER.myRouter.post('/register', ROUTER.processing.register);
 
 //栏目接口，返回所有栏目
-ROUTER.myRouter.get('/category', async (req, res) => {
-    let message = {statusCode: 400, message: '服务器繁忙，请稍后再试'};
-    message = await ROUTER.processing.category();
-    res.send(message);
-});
+ROUTER.myRouter.get('/category', ROUTER.processing.category);
 
 //新闻列表接口，返回对应栏目的新闻
-ROUTER.myRouter.get('/newslist', async (req, res) => {
-    let message = {statusCode: 400, message: '服务器繁忙，请稍后再试'};
-    message = await ROUTER.processing.getNewList(req.query);
-    res.send(message);
-});
+ROUTER.myRouter.get('/newslist', ROUTER.processing.getNewList);
 
 //用户详情接口
-ROUTER.myRouter.get('/userdetail', async (req, res) => {
-    let message = {statusCode: 400, message: '服务器繁忙，请稍后再试'};
-    message = await ROUTER.processing.getUserDetails(req.headers);
-    res.send(message);
-});
+ROUTER.myRouter.get('/userdetail', ROUTER.processing.getUserDetails);
 
 //用户信息编辑接口
-ROUTER.myRouter.post('/useredit', async (req, res) => {
-    let message = {statusCode: 400, message: '服务器繁忙，请稍后再试'};
-    message = await ROUTER.processing.editUserInfo(req.headers, req.body);
-    res.send(message);
-});
+ROUTER.myRouter.post('/useredit', ROUTER.processing.editUserInfo);
 
 //修改密码接口
-ROUTER.myRouter.post('/updatepwd', async (req, res) => {
-    let message = {statusCode: 400, message: '服务器繁忙，请稍后再试'};
-    message = await ROUTER.processing.updatePwd(req.headers, req.body);
-    res.send(message);
-});
+ROUTER.myRouter.post('/updatepwd', ROUTER.processing.updatePwd);
 
 //上传文件接口
 ROUTER.myRouter.post('/uploadfile', ROUTER.processing.uploadImg);
-
-// async (req, res) => {
-//     let message = {statusCode: 308, message: '图片上传错误'};
-//     message = await ROUTER.processing.uploadImg(req);
-//     res.send(message);
-// }
 
 //导出路由实例
 module.exports = ROUTER.myRouter;
