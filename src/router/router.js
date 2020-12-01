05599a20a367affb1b98215e18935ee0b7ac5076
万松涛 <2294215581@qq.com>
2020-12-01 09:39:38 +0800

    修改路由类，书写更规范

--- src/router/router.js
+++ src/router/router.js
@@ -4,51 +4,59 @@
  * @date 2020-11
  */
 class Router {
-    express = require('express');
-    path = require('path');
-    processing = require('../controller/processing');
-
-    /**
-     * @description 路由实例
-     */
-    myRouter = this.express.Router();
 
     constructor() { 
+        const express = require('express');
+        const path = require('path');
+        const processing = require('../controller/processing');
+
+        /**
+         * @description 路由实例
+         */
+        this.myRouter = express.Router();
+
         //返回静态资源
-        this.myRouter.use('/upload', this.express.static(this.path.join(__dirname, '../../src/upload')));
+        this.myRouter.use('/upload', express.static(path.join(__dirname, '../../src/upload')));
 
         //定义登录接口
-        this.myRouter.post('/login', this.processing.login);
+        this.myRouter.post('/login', processing.login);
 
         //定义注册接口
-        this.myRouter.post('/register', this.processing.register);
+        this.myRouter.post('/register', processing.register);
 
         //栏目接口，返回所有栏目
-        this.myRouter.get('/category', this.processing.category);
+        this.myRouter.get('/category', processing.category);
 
         //新闻列表接口，返回对应栏目的新闻
-        this.myRouter.get('/newslist', this.processing.getNewList);
+        this.myRouter.get('/newslist', processing.getNewList);
 
         //用户详情接口
-        this.myRouter.get('/userdetail', this.processing.getUserDetails);
+        this.myRouter.get('/userdetail', processing.getUserDetails);
 
         //用户信息编辑接口
-        this.myRouter.post('/useredit', this.processing.editUserInfo);
+        this.myRouter.post('/useredit', processing.editUserInfo);
 
         //修改密码接口
-        this.myRouter.post('/updatepwd', this.processing.updatePwd);
+        this.myRouter.post('/updatepwd', processing.updatePwd);
 
         //上传文件接口
-        this.myRouter.post('/uploadfile', this.processing.uploadImg);
+        this.myRouter.post('/uploadfile', processing.uploadImg);
 
         //热门新闻接口
-        this.myRouter.get('/hotnews', this.processing.getHotNews);
+        this.myRouter.get('/hotnews', processing.getHotNews);
 
         //搜索新闻接口
-        this.myRouter.get('/searchnews', this.processing.searchNews);
+        this.myRouter.get('/searchnews', processing.searchNews);
 
         //新闻预查询接口
-        this.myRouter.get('/beforesearch', this.processing.beforeSearch);
+        this.myRouter.get('/beforesearch', processing.beforeSearch);
+    }
+
+    /**
+     * @description 返回路由实例
+     */
+    backRouter() {
+        return this.myRouter;
     }
 }
 
@@ -56,4 +64,4 @@ class Router {
 const ROUTERS = new Router();
 
 //导出路由实例
-module.exports = ROUTERS.myRouter;
+module.exports = ROUTERS.backRouter();
