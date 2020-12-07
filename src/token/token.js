e224797939b40c9879b1576ea34fd3bf7c94bd0d
万松涛 <2294215581@qq.com>
2020-12-07 12:21:00 +0800

    修改token的key值

--- src/token/token.js
+++ src/token/token.js
@@ -5,7 +5,17 @@
  */
 class Token {
     static jsonwebtoken = require('jsonwebtoken');
-    static key = 'wansongtao';
+
+    /**
+     * @description 生成key值
+     * @returns 返回一个独一无二的key值
+     */
+    static key () {
+        let num1 = Math.ceil(Math.random() * 10000 + 1000);
+        let num2 = Math.floor(Math.random() * 1000 + 888);
+
+        return 'wansongtao' + num1.toString() + num2.toString();
+    }
 
     /**
      * @description 创建token
@@ -25,7 +35,7 @@ class Token {
             //生成token并将用户id存入其中，过期时间4小时
             myToken = this.jsonwebtoken.sign({
                 userId
-            }, this.key, {
+            }, this.key(), {
                 expiresIn: '4h'
             });
         } catch (ex) {
@@ -39,7 +49,7 @@ class Token {
     /**
      * @description 验证token
      * @param {string} tokens 生成的token字符串
-     * @return 验证成功返回用户id，失败返回false
+     * @return 验证成功返回用户id，失败返回false，如果是token错误返回0
      */
     static verifyToken(tokens) {
         let data = '';
@@ -51,10 +61,18 @@ class Token {
                 };
             }
     
-            data = this.jsonwebtoken.verify(tokens, this.key);
-            data = data.userId;
+            data = this.jsonwebtoken.verify(tokens, this.key());
+            data = data.userId;  
         } catch (ex) {
-            data = false;
+            if (ex.message == 'invalid signature') {
+                //invalid signature token错误
+                data = 0;
+            }
+            else {
+                //jwt expired token超时错误
+                data = false;
+            }
+            
             console.error('Class Token => verifyToken(): ' + ex.message);
         } finally {
             return data;
